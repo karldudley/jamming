a1dc5a0e65e5976b17263579458ac43382e1784e
Karl Dudley <karldudley@gmail.com>
2024-10-16 21:00:29 +0100

    update redirect

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
 // src/util/Spotify.js
 const clientId = process.env.REACT_APP_CLIENT_ID; // Replace with your Spotify Client ID
-const redirectUri = 'http://localhost:5173/callback'; // Replace with your redirect URI
+const redirectUri = 'https://jamming-kd.vercel.app/callback'; // Replace with your redirect URI
 
 let accessToken;
 
